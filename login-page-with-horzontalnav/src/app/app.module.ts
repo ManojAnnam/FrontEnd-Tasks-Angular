0836c5c4102f67d88cff740c9b64ef2e81af8022
Manoj Annam <manoj.annam@ggktech.com>
2019-09-09 14:59:05 +0530

    Responsive Horizontal Navigation Bar

--- login-page-with-horzontalnav/src/app/app.module.ts
+++ login-page-with-horzontalnav/src/app/app.module.ts
@@ -1,29 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponentComponent } from './login-component/login-component.component';
-import {MatButtonModule} from '@angular/material/button';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material';
+import { MatCardModule } from '@angular/material/card';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 @NgModule({
-   declarations: [
-      AppComponent,
-      LoginComponentComponent
-   ],
-   imports: [
-      BrowserModule,
-      BrowserAnimationsModule,
-      AppRoutingModule,
-      MatButtonModule,
-      MatFormFieldModule,
-      MatInputModule
-   ],
-   providers: [],
-   bootstrap: [
-      AppComponent
-   ]
+  declarations: [AppComponent, LoginComponentComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    AngularFontAwesomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
